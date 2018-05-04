95d40333d9e7308644375c95a5a72294c17ef367
Uboat46 <uboat46@hotmail.com>
2018-05-04 01:14:14 -0500

    two way binding

--- vue-todo-list/src/components/TodoList.vue
+++ vue-todo-list/src/components/TodoList.vue
@@ -5,22 +5,31 @@
         A Vue Simple Todo List
       </div>
     </header>
+
     <section class="w3-container w3-border w3-border-grey w3-center w3-padding">
-      <input type="text" class="w3-input w3-border w3-half w3-margin w3-large">
+      <input type="text" class="w3-input w3-border w3-half w3-margin w3-large" v-model="newTodo">
       <button class="w3-button w3-green w3-round w3-margin w3-large">Add todo</button>
+      <p class="w3-center w3-text-grey">{{newTodo}}</p>
     </section>
+
     <section class="w3-container w3-border w3-border-grey">
       <h3>ToDo´s</h3>
     </section>
+
     <section class="w3-container w3-border w3-border-grey">
       <h3>Done</h3>
     </section>
+
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      newTodo: ''
+    }
+  }
 }
 </script>
 
